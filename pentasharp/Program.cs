2603e5c0e7cdb8f4206f79259bbb8f79db7496e9
Rinesa <99516057+rinesakuci@users.noreply.github.com>
2024-10-29 12:32:21 +0100

    Setup DbSet in AppDbContext

--- pentasharp/Program.cs
+++ pentasharp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using pentasharp.Data;
+
 namespace WebApplication1
 {
     public class Program
@@ -8,6 +11,9 @@ namespace WebApplication1
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
