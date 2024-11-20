fd18f77fc6336709b19c1d31c811adcb91792acc
oltahasimja <oh67751@ubt-uni.net>
2024-11-20 10:35:09 +0100

    Add new BusViewModel and ManageBusCompanyViewModel, apply stashed changes

---
+++ pentasharp/ViewModel/Bus/BusViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace pentasharp.ViewModel.Bus
+{
+    public class BusViewModel
+    {
+        public int BusId { get; set; }
+        public int BusNumber { get; set; }
+        public int Capacity { get; set; }
+        public int BusCompanyId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
